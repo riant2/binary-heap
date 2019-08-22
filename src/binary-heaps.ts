05010883c1606854cd992cdf06ece65cfa01c213
Rickard Antonsson <rickard.antonsson@ikea.com>
2019-08-22 22:07:35 +0200

    Initial commit

--- src/binary-heaps.ts
+++ src/binary-heaps.ts
@@ -1,87 +1,96 @@
 import { OneBasedArray } from './heap';
 
+export const positiveSort = (a: number, b: number) => (a >= b ? a : b);
+export const negativeSort = (a: number, b: number) => (a <= b ? a : b);
+
 export class BinaryHeap<T> {
-  private heap = new OneBasedArray<T>();
+	private heap = new OneBasedArray<T>();
 
-  constructor(
-    private sortBy: (a: T) => number,
-    private math: (a: number, b: number) => number = Math.max
-  ) {}
+	constructor(
+		private sortBy: (a: T) => number,
+		private math: (a: number, b: number) => number = Math.max
+	) {}
 
-  get empty(): boolean {
-    return this.heap.length < 1;
-  }
+	get empty(): boolean {
+		return this.heap.length < 1;
+	}
 
-  push = (...objects: T[]) => {
-    objects.forEach(object => this._push(object));
-  };
+	get peek(): T {
+		return this.heap.get(1);
+	}
 
-  pull = (): T => {
-    const { heap, better, swap } = this,
-      object = heap.get(1),
-      last = heap.pop();
+	push = (object: T | T[]) => {
+		if (Array.isArray(object)) {
+			return object.forEach(this._push);
+		}
+		this._push(object);
+	};
 
-    if (!last) throw new Error('Nothing to pull');
-    if (heap.length < 1) return object;
+	pull = (): T => {
+		const { heap, better, swap } = this,
+			object = heap.get(1),
+			last = heap.pop();
+		if (last === undefined) throw new Error('Nothing to pull');
+		if (heap.length === 0) return object;
 
-    let lastPosition = 1;
-    heap.set(lastPosition, last);
-    while (true) {
-      const first = lastPosition * 2,
-        second = lastPosition * 2 + 1,
-        best = better(first, second);
+		heap.set(1, last);
+		let lastPosition = 1;
+		while (true) {
+			const first = lastPosition << 1,
+				second = (lastPosition << 1) + 1,
+				best = better(first, second);
 
-      if (better(best, lastPosition) === best) {
-        swap(lastPosition, best);
-        lastPosition = best;
-        continue;
-      }
-      break;
-    }
+			if (better(best, lastPosition) === best) {
+				swap(lastPosition, best);
+				lastPosition = best;
+				continue;
+			}
+			break;
+		}
 
-    return object;
-  };
+		return object;
+	};
 
-  pullAll = (): T[] => {
-    const values: T[] = [];
-    while (!this.empty) {
-      values.push(this.pull());
-    }
-    return values;
-  };
+	pullAll = (): T[] => {
+		const values: T[] = [];
+		while (!this.empty) {
+			values.push(this.pull());
+		}
+		return values;
+	};
 
-  private better = (a: number, b: number): number => {
-    const av = this.sortBy(this.heap.get(a)),
-      bv = this.sortBy(this.heap.get(b));
-    if (typeof bv === 'undefined') return a;
-    return this.math(av, bv) === av ? a : b;
-  };
+	private better = (a: number, b: number): number => {
+		if (b === undefined) return a;
+		const av = this.sortBy(this.heap.get(a)),
+			bv = this.sortBy(this.heap.get(b));
+		return this.math(av, bv) === av ? a : b;
+	};
 
-  private _push = (object: T) => {
-    const { heap, better, swap } = this;
+	private _push = (object: T) => {
+		const { heap, better, swap } = this;
 
-    let p1 = heap.length + 1;
-    heap.set(p1, object);
+		let p1 = heap.length + 1;
+		heap.set(p1, object);
 
-    if (p1 === 1) return;
+		if (p1 === 1) return;
 
-    while (p1 > 0) {
-      const p2 = Math.floor(p1 / 2);
-      if (p2 > 0 && better(p1, p2) === p1) {
-        swap(p1, p2);
-        p1 = p2;
-      } else break;
-    }
-  };
+		while (p1 > 1) {
+			const p2 = p1 >> 1;
+			if (better(p1, p2) === p1) {
+				swap(p1, p2);
+				p1 = p2;
+			} else break;
+		}
+	};
 
-  // p = position, o = object
-  private swap = (p1: number, p2: number) => {
-    const {
-        heap: { get, set },
-      } = this,
-      o1 = get(p1),
-      o2 = get(p2);
-    set(p1, o2);
-    set(p2, o1);
-  };
+	// p = position, o = object
+	private swap = (p1: number, p2: number) => {
+		const {
+				heap: { get, set },
+			} = this,
+			o1 = get(p1),
+			o2 = get(p2);
+		set(p1, o2);
+		set(p2, o1);
+	};
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,47 @@
-import { BinaryHeap } from './binary-heaps';
+import { performance } from 'perf_hooks';
+import { BinaryHeap, negativeSort, positiveSort } from './binary-heaps';
+
+const randomNumbers = getRandomNumbers(1000000);
 
 async function main(best: (a: number, b: number) => number) {
-  const bh = new BinaryHeap<number>(val => val, best);
+	const bh = new BinaryHeap<number>(val => val, best);
 
-  for (let i = 0; i < 10; i++) {
-    bh.push(random(-100, 100));
-  }
+	const t1 = performance.now();
+	bh.push(randomNumbers);
+	const t2 = performance.now();
+	const first = bh.pull();
+	const t3 = performance.now();
+	const second = bh.pull();
+	const t4 = performance.now();
+	const values = bh.pullAll();
+	const t5 = performance.now();
 
-  const values = bh.pullAll();
-  console.log(values);
+	console.log(
+		`push took:${t2 - t1}, pull took: ${t3 - t2}, second pull took: ${t4 -
+			t3}, pullAll took: ${t5 - t4}`
+	);
 }
 
 function random(min: number, max: number) {
-  return Math.floor(Math.random() * (max - min) + min);
+	return Math.floor(Math.random() * (max + 1 - min) + min);
 }
 
-main(Math.max).catch(console.error);
-main(Math.min).catch(console.error);
+function getRandomNumbers(amount: number): number[] {
+	const numbers: number[] = [];
+	for (let i = 0; i < amount; i++) {
+		numbers.push(random(0, 100));
+	}
+	return numbers;
+}
+
+function testNormalSort() {
+	const numbers = getRandomNumbers(10000000);
+	const t1 = performance.now();
+	numbers.sort((a, b) => a - b);
+	const t2 = performance.now();
+	console.log(`sort took:${t2 - t1}`);
+}
+
+main(positiveSort).catch(console.error);
+main(negativeSort).catch(console.error);
+testNormalSort();
